3d5a70f0719ef6fa34cd6d77048c2142ec791c7e
pigff <androiddevzf@163.com>
2018-03-17 16:35:55 +0800

    电话广播、屏幕广播、sms广播、时间广播 轮询工具类。。等等

--- utils/src/main/java/com/linjianlin/utils/blankj/ScreenUtils.java
+++ utils/src/main/java/com/linjianlin/utils/blankj/ScreenUtils.java
@@ -43,8 +43,10 @@ public final class ScreenUtils {
      */
     public static int getScreenWidth() {
         WindowManager windowManager = (WindowManager) Utils.getContext().getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics dm = new DisplayMetrics();// 创建了一张白纸
-        windowManager.getDefaultDisplay().getMetrics(dm);// 给白纸设置宽高
+        // 创建了一张白纸
+        DisplayMetrics dm = new DisplayMetrics();
+        // 给白纸设置宽高
+        windowManager.getDefaultDisplay().getMetrics(dm);
         return dm.widthPixels;
     }
 
@@ -55,8 +57,10 @@ public final class ScreenUtils {
      */
     public static int getScreenHeight() {
         WindowManager windowManager = (WindowManager) Utils.getContext().getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics dm = new DisplayMetrics();// 创建了一张白纸
-        windowManager.getDefaultDisplay().getMetrics(dm);// 给白纸设置宽高
+        // 创建了一张白纸
+        DisplayMetrics dm = new DisplayMetrics();
+        // 给白纸设置宽高
+        windowManager.getDefaultDisplay().getMetrics(dm);
         return dm.heightPixels;
     }
 
